7fa7be3835ec3647044c643dabeacaea6cb265b9
sahil3754 <109479393+sahil3754@users.noreply.github.com>
2024-05-24 17:00:28 +0530

    Update ItemListComponent.jsx

--- src/Components/ItemListComponent/ItemListComponent.jsx
+++ src/Components/ItemListComponent/ItemListComponent.jsx
@@ -42,18 +42,7 @@ const ItemListComponent = () => {
                 {editId === myEdit?.id ? (
                     <>
 
-                        {/* <input
-                                type="email"
-                                value={editEmail}
-                                onChange={(e) => setEditEmail(e.target.value)}
-                                placeholder="Email"
-                            />
-                            <input
-                                type="text"
-                                value={editContact}
-                                onChange={(e) => setEditContact(e.target.value)}
-                                placeholder="Contact"
-                            /> */}
+                       
 
                         <div class="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24 myFont">
                             <div class="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
